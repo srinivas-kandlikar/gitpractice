321a502760ad00f3fadcb65a1313837ba71073f8
srinivas kandlikar <srinivas.us231@gmail.com>
2020-05-18 19:58:44 +0530

    sdasfasff  Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch master
    
     Initial commit
    
     Changes to be committed:
    	new file:   example1.html
    	new file:   js/custom.js

---
+++ js/custom.js
@@ -0,0 +1,110 @@
+/*alert('srinivas');
+console.log('adasfa');
+
+var mame;
+var age;
+name="srinivas";
+console.log(name);
+var firstname = 'srinivas';
+var age = 26;
+console.log(firstname + ' ' + age);
+*/
+
+/*
+prompt exmaple
+var job, isMarried;
+
+job = "teacher";
+isMarried = false;
+var lastname = prompt('what is your last name ?');
+alert(firstname + lastname + ' is a' + age + ' year old ' + job +' is he married ?' + isMarried); 
+*/
+
+/*year exmple dob
+var year =2018;
+yourage = prompt('enter your age ?');
+var age = year - yourage;
+console.log(age);
+*/
+ 
+ /*year example age 
+year = 2019
+yourage = prompt("enter you dob?");
+console.log( year - yourage);
+year = 2019;
+abc = "srinibas";
+yoursage = prompt('enter your age ? ');
+console.log(year - yoursage);*/
+
+/*type of
+
+console.log(typeof abc); */
+
+/* age console age minmimum 18
+var present = 2019;
+var yourage = 1994;
+var minage = 18;
+var total = present - yourage >= minage;
+console.log(total);*/
+
+/* alert age minimum 18
+var present = 2019;
+var yourage = prompt ('enter your dob ?');
+var minage = 18;
+var total = present - yourage >= minage;
+alert(total); */
+
+
+/*baisc bmi
+var kg = prompt ('enter your weight ?');
+ height  = prompt ('enter you height ?')
+var bmi = kg / height * 2;
+console.log(bmi); */
+
+/*first example of bmi calculator
+
+var markweight,markheight,markbmi;
+var markweight  = prompt('enter mark weight ?');
+var markheight = prompt('enter mark height ?');
+ var markbmi = markweight / markheight * markheight;
+
+
+
+var johnweight,johnheight,johnbmi;
+var johnweight  = prompt('enter john weight ?');
+var johnheight = prompt('enter john height ?');
+ var johnbmi = johnweight / johnheight * johnheight;
+
+
+var total = markbmi > johnbmi;
+alert ( 'is mark bmi is greatere than john? ' + total);
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
